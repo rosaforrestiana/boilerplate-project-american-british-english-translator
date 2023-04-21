3ea23d36f9f05477ec4764ea449fa92c0d8ddb64
rosaforrestiana <30365715+rosaforrestiana@users.noreply.github.com>
2023-04-21 12:12:43 +0800

    Updated 21st April 2023

--- routes/api.js
+++ routes/api.js
@@ -8,6 +8,22 @@ module.exports = function (app) {
 
   app.route('/api/translate')
     .post((req, res) => {
-      
+      //console.log(req.body)
+      const{text,locale}=req.body;
+      if(!locale||text === undefined){
+       return res.json({error:'Required field(s) missing'})
+      }
+      if(text === ''){
+        return res.json({error:'No text to translate'})
+      }
+
+      if(locale !== 'american-to-british' && locale !== 'british-to-american'){
+        return res.json({error:'Invalid value for locale field'})
+      }
+
+      if(locale|text !== undefined){
+      //console.log(translator.returnMe(text))
+      return res.json({text:text,translation:translator.returnMe(text,locale)})
+      }
     });
 };
